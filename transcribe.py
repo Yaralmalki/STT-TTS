2dff38c749e478f3a3a954d06e0ba1942171bc01
Yara Almalki <71742682+Yaralmalki@users.noreply.github.com>
2021-07-08 16:53:53 -0700

    Update transcribe.py

--- transcribe.py
+++ transcribe.py
@@ -96,6 +96,8 @@ def on_message(self, msg):
             LAST = data
         # This prints out the current fragment that we are working on
         print(data['results'][0]['alternatives'][0]['transcript'])
+        with open('outext.txt', 'w') as output:
+           output.writelines(data['results'][0]['alternatives'][0]['transcript'])
 
 
 def on_error(self, error):
